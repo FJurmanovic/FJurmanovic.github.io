535fd00b71c546d063dcd76457d3e8e2bce31738
EC2 Default User <ec2-user@ip-172-31-34-149.eu-central-1.compute.internal>
2019-09-24 14:36:59 +0000

    edit

--- tomo.js
+++ tomo.js
@@ -1,22 +1,14 @@
 function get_hehe(){
-    document.getElementById('hehe').style.visibility = "visible";
-    document.getElementById('hihi').style.visibility = "hidden";
-    document.getElementById('hoho').style.visibility = "hidden";
+    document.getElementById('hehe').innerHTML = "Tomo je feggatino";
 }
 function get_hihi(){
-    document.getElementById('hehe').style.visibility = "hidden";
-    document.getElementById('hihi').style.visibility = "visible";
-    document.getElementById('hoho').style.visibility = "hidden";
+    document.getElementById('hehe').innerHTML = "Flutter je bolji";
 }
 function get_hoho(){
-    document.getElementById('hehe').style.visibility = "hidden";
-    document.getElementById('hihi').style.visibility = "hidden";
-    document.getElementById('hoho').style.visibility = "visible";
+    document.getElementById('hehe').innerHTML = "Kotlin bad";
 }
 window.onload = function(){
-    while(true){
-        setTimeout("get_hehe()", 3000);
-        setTimeout("get_hihi()", 3000);
-        setTimeout("get_hoho()", 3000);
-    }
+    setTimeout("get_hehe()", 3000);
+    setTimeout("get_hihi()", 3000);
+    setTimeout("get_hoho()", 3000);
 }
